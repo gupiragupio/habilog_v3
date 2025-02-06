a42421813f9b75d4e21d6b4e9e74c28e171ff985
gupiragupio <suzukikouhei2020@gmail.com>
2025-02-06 20:25:27 +0900

    マイグレーションファイルの作成

--- db/migrate/20250206110844_create_habits.rb
+++ db/migrate/20250206110844_create_habits.rb
@@ -1,7 +1,9 @@
-class CreateHabits < ActiveRecord::Migration[7.1]
+class CreateHabits < ActiveRecord::Migration[7.0]
   def change
     create_table :habits do |t|
-
+      t.references :user, null: false ,foreign_key: true
+      t.string :title, null: false
+      t.text :description
       t.timestamps
     end
   end
