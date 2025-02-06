53318791367007e1ed5884453fc8f20489a9261e
gupiragupio <suzukikouhei2020@gmail.com>
2025-02-06 20:34:50 +0900

    ビューモデル、コントローらー作成

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,2 +1,6 @@
 class Habit < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :description, length: { maximum: 1000 }, allow_blank: true
 end
