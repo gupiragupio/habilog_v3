2a6f806490816d52e90d531e895a3f5e5a13fe9b
gupiragupio <suzukikouhei2020@gmail.com>
2025-02-07 19:26:56 +0900

    マイグレーションファイルの作り直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_06_111445) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_07_110844) do
   create_table "habits", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "title", null: false
